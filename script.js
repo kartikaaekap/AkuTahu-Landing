db2fd5cd013c4c0c14f87c3b6864aaaeefc63eee
kartikaaekap <kartikaaekap@gmail.com>
2021-04-01 08:36:02 +0700

    update again

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 /* Side-Navbar */
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "250px";
-  }
+  document.getElementById("mySidepanel").style.width = "250px";
+}
 
 function closeNav() {
-  document.getElementById("mySidepanel").style.width = "0";
+document.getElementById("mySidepanel").style.width = "0";
 }
 
 /* Sliding/click Berita Populer */
@@ -28,19 +28,19 @@ function closeNav() {
 
 /* Sliding/click Berita Lainnya */
 function openOther(evt, OtherCategory) {
-  var i, othertabcontent, othertablinks;
+var i, othertabcontent, othertablinks;
 
-  othertabcontent = document.getElementsByClassName("othertabcontent");
-  for (i = 0; i < othertabcontent.length; i++) {
-    othertabcontent[i].style.display = "none";
-  }
+othertabcontent = document.getElementsByClassName("othertabcontent");
+for (i = 0; i < othertabcontent.length; i++) {
+  othertabcontent[i].style.display = "none";
+}
 
-  othertablinks = document.getElementsByClassName("othertablinks");
-  for (i = 0; i < othertablinks.length; i++) {
-    othertablinks[i].className = othertablinks[i].className.replace(" active", "");
-  }
+othertablinks = document.getElementsByClassName("othertablinks");
+for (i = 0; i < othertablinks.length; i++) {
+  othertablinks[i].className = othertablinks[i].className.replace(" active", "");
+}
 
-  document.getElementById(OtherCategory).style.display = "block";
-  evt.currentTarget.className += " active";
+document.getElementById(OtherCategory).style.display = "block";
+evt.currentTarget.className += " active";
 }
 document.getElementById("defaultOpenOther").click();
